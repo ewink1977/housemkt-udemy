3f1f452e134bd8c52c85835d050cfec7a7320607
Erin Winking <erin@douglasavenue.com>
2022-04-29 17:53:23 -0500

    Added functionality to the sign up page, users now added to db

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	updateProfile,
+} from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -12,13 +19,41 @@ function SignUp() {
 	})
 	const { email, password, name } = formData
 
-	const navigate = useNavigate
+	const navigate = useNavigate()
 
 	const onChange = (e) => {
 		setFormData((prevState) => ({
 			...prevState,
 			[e.target.id]: e.target.value,
 		}))
+	}
+
+	const onSubmit = async (e) => {
+		e.preventDefault()
+
+		try {
+			const auth = getAuth()
+
+			const userCredential = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password
+			)
+
+			const user = userCredential.user
+
+			updateProfile(auth.currentUser, { displayName: name })
+
+			const formDataCopy = { ...formData }
+			delete formDataCopy.password
+			formDataCopy.timestamp = serverTimestamp()
+
+			await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+			navigate('/')
+		} catch (error) {
+			console.log(error)
+		}
 	}
 
 	return (
@@ -28,7 +63,7 @@ function SignUp() {
 					<p className='pageHeader'>Welcome Back</p>
 				</header>
 				<main>
-					<form>
+					<form onSubmit={onSubmit}>
 						<input
 							type='text'
 							className='nameInput'
